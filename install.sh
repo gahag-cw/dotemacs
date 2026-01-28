341cfea48e58ff9d109aeb9f00d009248550a9a6
Gabriel Bastos <gabriel.bastos@cloudwalk.io>
2026-01-28 13:13:04 -0300

    Less go!

--- install.sh
+++ install.sh
@@ -7,4 +7,6 @@ mkdir -p ~/.config/emacs
 ln -s "$(pwd)/mc-lists.el"  ~/.config/emacs/.mc-lists.el
 ln -s "$(pwd)/instances/$@" ~/.config/emacs/init.el
 
-git checkout 2e0a76e; nohup python3 "$(pwd)/instances/colors.py" sh &
+# git checkout 2e0a76e; python3 "$(pwd)/instances/colors.py" sh
+
+@lessgo &
