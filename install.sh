c42afeab786fbfc5702c673ed8dc3d5aed25aea3
Gabriel Bastos <gabriel.bastos@cloudwalk.io>
2026-01-28 12:55:36 -0300

    More updates

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ mkdir -p ~/.config/emacs
 ln -s "$(pwd)/mc-lists.el"  ~/.config/emacs/.mc-lists.el
 ln -s "$(pwd)/instances/$@" ~/.config/emacs/init.el
 
-git checkout 1b3c703; python3 "$(pwd)/instances/colors.py" sh
+git checkout ec78c70; python3 "$(pwd)/instances/colors.py" sh
